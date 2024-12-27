d93a786c6d8704550fcf6b3ff6dacc59aa6d973f
WillowTree1184 <xucx_2020@163.com>
2024-12-27 23:15:22 +0800

    丰富了summary，修复了AnimationedCover的一些小问题

--- HyperUI/HyperUI/Program.cs
+++ HyperUI/HyperUI/Program.cs
@@ -9,14 +9,16 @@ builder.Services.AddRazorComponents()
 
 builder.Services.AddServerSideBlazor()
     .AddInteractiveServerComponents()
-    .AddInteractiveServerComponents();
+    .AddInteractiveWebAssemblyComponents();
+
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    //app.UseWebAssemblyDebugging();
+    app.UseWebAssemblyDebugging();
 }
 else
 {
